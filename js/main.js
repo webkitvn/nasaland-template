20a87d102fd82d64245b4105431be6d3238bad7b
Cuong Pham <Cuong Pham>
2017-02-18 16:41:58 +0700

    fix nav bar menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function(){
     jQuery('#btn-search').click(function(){
         jQuery(this).toggleClass('active');
-        jQuery('#searchform').toggleClass('active')
+        jQuery('#searchform').toggleClass('active');
+        jQuery('#searchform input[type=search]').focus();
         return false;
     })
 })
