04222e21b4e417be79d637a6114c14a6fa65ec2e
Cuong Pham <Cuong Pham>
2017-03-20 22:01:04 +0700

    Tao landing page can ho

--- js/main.js
+++ js/main.js
@@ -1,15 +1,43 @@
-jQuery(document).ready(function(){
-    jQuery('#btn-search').click(function(){
-        jQuery(this).toggleClass('active');
-        jQuery('#searchform').toggleClass('active');
-        jQuery('#searchform input[type=search]').focus();
+$(document).ready(function() {
+    $('#btn-search').click(function() {
+        $(this).toggleClass('active');
+        $('#searchform').toggleClass('active');
+        $('#searchform input[type=search]').focus();
         return false;
     })
+
+    $('.scrollbar-outer').scrollbar();
+
+    //FULL DIV SLIDER
+    var $item = $('.carousel .item');
+    var $wHeight = $('.full-height').height();
+    $item.eq(0).addClass('active');
+    $item.height($wHeight);
+    $item.addClass('full-screen');
+
+    $('.carousel img').each(function() {
+        var $src = $(this).attr('src');
+        var $color = $(this).attr('data-color');
+        $(this).parent().css({
+            'background-image': 'url(' + $src + ')',
+            'background-color': $color
+        });
+        $(this).remove();
+    });
+
+    $(window).on('resize', function() {
+        $wHeight = $(window).height();
+        $item.height($wHeight);
+    });
+
+    $('.carousel').carousel({
+        interval: 6000,
+        pause: "false"
+    });
 })
-jQuery('.ui.dropdown')
-    .dropdown()
-;
-jQuery('a#advance-search').click(function(){
-    jQuery('.advance-search').toggleClass('hidden');
+$('.ui.dropdown')
+    .dropdown();
+$('a#advance-search').click(function() {
+    $('.advance-search').toggleClass('hidden');
     return false;
 })
